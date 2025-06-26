74a05955574e847f4cf7ff7095d2646a5986012a
Ozmanov Alik <alik.ozmanov@fms-ea.com>
2025-06-26 13:28:36 +0200

    entitiés, repositories (review et reservation)

---
+++ super-hotel/src/main/java/fr/fms/entities/Review.java
@@ -0,0 +1,25 @@
+package fr.fms.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int rating;
+
+    private String comment;
+
+    @ManyToOne
+    private Hotel hotel;
+
+}
